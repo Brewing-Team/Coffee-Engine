d6ad9bdc3ba3e0aca993ee4df995482282197de8
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2024-09-16 23:00:53 +0200

    Merge branch 'heads/doc'

--- TeaEngine/src/TeaEngine/ImGui/ImGuiLayer.h
+++ TeaEngine/src/TeaEngine/ImGui/ImGuiLayer.h
@@ -6,76 +6,76 @@ namespace Tea {
 
     /**
      * @defgroup imgui ImGui
+     * @brief ImGui components of the TeaEngine.
      * @{
      */
 
     /**
-     * @brief Layer for ImGui integration.
-     * @ingroup imgui
+     * @brief The ImGuiLayer class is responsible for managing the ImGui layer in the application.
      */
     class ImGuiLayer : public Layer
     {
     public:
         /**
-         * @brief Constructor for ImGuiLayer.
+         * @brief Constructs the ImGuiLayer object.
          */
         ImGuiLayer();
 
         /**
-         * @brief Destructor for ImGuiLayer.
+         * @brief Destroys the ImGuiLayer object.
          */
         ~ImGuiLayer();
 
         /**
-         * @brief Called when the layer is attached.
+         * @brief Attaches the ImGui layer to the application.
          */
         void OnAttach() override;
 
         /**
-         * @brief Called when the layer is detached.
+         * @brief Detaches the ImGui layer from the application.
          */
         void OnDetach() override;
 
         /**
-         * @brief Called when an event occurs.
-         * @param e The event.
+         * @brief Handles events for the ImGui layer.
+         * @param e The event to handle.
          */
         void OnEvent(Event& e) override;
 
         /**
-         * @brief Begin ImGui frame.
+         * @brief Begins a new ImGui frame.
          */
         void Begin();
 
         /**
-         * @brief End ImGui frame.
+         * @brief Ends the current ImGui frame.
          */
         void End();
 
         /**
-         * @brief Called to render ImGui.
+         * @brief Renders ImGui elements.
          */
         void OnImGuiRender() override;
 
         /**
-         * @brief Block or unblock events.
-         * @param block Whether to block events.
+         * @brief Blocks or unblocks events from being handled by the ImGui layer.
+         * @param block True to block events, false to unblock.
          */
         void BlockEvents(bool block) { m_BlockEvents = block; }
 
         /**
-         * @brief Set the Tea color style for ImGui.
+         * @brief Sets the Tea color style for ImGui.
          */
         void SetTeaColorStyle();
 
         /**
-         * @brief Set the Godot color style for ImGui.
+         * @brief Sets the Godot color style for ImGui.
          */
         void SetGodotColorStyle();
 
     private:
-        bool m_BlockEvents = true;
+        bool m_BlockEvents = true; ///< Indicates whether events are blocked.
     };
 
-    /** @} */ // end of imgui group
+    /** @} */
 }
