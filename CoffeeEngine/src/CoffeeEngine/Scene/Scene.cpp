66b680f97b58e86b34f43830ba0ff109f6958c5c
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2024-12-09 14:24:20 +0100

    entity bindings now prints errors
    Co-Authored-By: Hugo Planell <69766683+HugoPlacer@users.noreply.github.com>

--- CoffeeEngine/src/CoffeeEngine/Scene/Scene.cpp
+++ CoffeeEngine/src/CoffeeEngine/Scene/Scene.cpp
@@ -204,9 +204,10 @@ namespace Coffee {
         for (auto& entity : scriptView)
         {
             Entity scriptEntity{entity, this};
-            ScriptManager::RegisterVariable("self", (void*)&scriptEntity);
+            ScriptManager::RegisterVariable("entity", (void*)&scriptEntity);
 
             auto& scriptComponent = scriptView.get<ScriptComponent>(entity);
+
             scriptComponent.script.OnUpdate();
         }
 
