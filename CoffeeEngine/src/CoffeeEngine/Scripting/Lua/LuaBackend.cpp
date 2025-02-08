004aa638e4a016bd32d6aaf364dd3d01be471665
Hugo Planell <69766683+hugoplacer@users.noreply.github.com>
2025-02-08 20:14:21 +0100

    [Lua Scripting] Initial Support for Complex Types like Entities

--- CoffeeEngine/src/CoffeeEngine/Scripting/Lua/LuaBackend.cpp
+++ CoffeeEngine/src/CoffeeEngine/Scripting/Lua/LuaBackend.cpp
@@ -594,7 +594,6 @@ namespace Coffee {
         } catch (const sol::error& e) {
             COFFEE_CORE_ERROR("Lua: {0}", e.what());
         }
-        script.ParseScript();
     }
 
 } // namespace Coffee
