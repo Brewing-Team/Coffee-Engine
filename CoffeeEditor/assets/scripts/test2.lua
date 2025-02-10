137dea38aef2f1f3ce89fb0e6b6a9ffc29f82cfc
Guillem Alqueza Mayench <guillemalqueza@gmail.com>
2025-02-10 21:06:15 +0100

    Merge branch 'Brewing-Team:main' into Audio-Components

--- CoffeeEditor/assets/scripts/test2.lua
+++ CoffeeEditor/assets/scripts/test2.lua
@@ -5,7 +5,23 @@
 function OnReady()
 end
 
-function OnUpdate(delta)
+function OnUpdate()
+    --log("OnUpdate()")
+    if input.is_key_pressed(input.keycode.Space) then
+        log("SPACE")
+    end
+
+    if input.is_mouse_button_pressed(input.mousecode.Left) then
+        log("LEFT")
+        local x, y = input.get_mouse_position()
+        log("Mouse position: (" .. x .. ", " .. y .. ")")
+    end
+
+    log("BOOL 2: " .. tostring(exampleBool))
+    log("INT 2: " .. exampleInt)
+    log("FLOAT 2: " .. exampleFloat)
+    log("STRING 2: " .. exampleString)
+    return 1
 end
 
 function on_destroy()
