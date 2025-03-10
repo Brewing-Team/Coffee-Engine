48f9f1fc7ba3a36a7e6674eeae1b8ced2e5fc848
errico6 <125646224+errico6@users.noreply.github.com>
2025-03-10 16:29:42 +0100

    Loop funcional

--- CoffeeEngine/src/CoffeeEngine/ParticleManager/ParticleManager.cpp
+++ CoffeeEngine/src/CoffeeEngine/ParticleManager/ParticleManager.cpp
@@ -1,13 +1,12 @@
 #include "ParticleManager.h"
-#include <cstdlib>
-#include <ctime>   
 #include "CoffeeEngine/Scene/Components.h"
+#include <cstdlib>
+#include <ctime>
 
 namespace Coffee
 {
 
     Particle::Particle() : position(0.0f), direction(0.0f), color(1.0f), size(1.0f), lifetime(1.0f) {}
-
 
     void Particle::Update(float dt)
     {
@@ -15,11 +14,10 @@ namespace Coffee
         lifetime -= dt;
     }
 
-
     void ParticleEmitter::InitParticle(Ref<Particle> p)
     {
         p->position = glm::vec3(0.0f);
-        p->direction = direction; // Ya no es un puntero
+        p->direction = direction;
         p->color = colourNormal;
         p->size = size;
         p->lifetime = lifeTime;
@@ -36,17 +34,21 @@ namespace Coffee
     {
         float dt = 0.16f;
         elapsedTime += dt;
+        timetotal += dt;
 
-        while (elapsedTime > rateOverTime)
+        if (looping || elapsedTime < lifeTime)
         {
-            GenerateParticle();
-            elapsedTime -= rateOverTime;
+            while (elapsedTime > rateOverTime)
+            {
+                GenerateParticle();
+                elapsedTime -= rateOverTime;
+            }
         }
 
         for (size_t i = 0; i < activeParticles.size();)
         {
             activeParticles[i]->Update(dt);
-            if (activeParticles.size() > amount)
+            if (activeParticles[i]->lifetime <= 0)
             {
                 activeParticles.erase(activeParticles.begin() + i);
             }
@@ -56,15 +58,15 @@ namespace Coffee
             }
         }
 
+        if (!looping && timetotal >= lifeTime )
+        {
+            // Detener generación de partículas si no es en bucle y ya han expirado
+            return;
+        }
 
-        printf("Cantidad particulas: %d", activeParticles.size());
-
-
+        printf("Cantidad particulas: %d", (int)activeParticles.size());
     }
 
-    void ParticleEmitter::Render()
-    {
-        
-    }
+    void ParticleEmitter::Render() {}
 
 } // namespace Coffee
