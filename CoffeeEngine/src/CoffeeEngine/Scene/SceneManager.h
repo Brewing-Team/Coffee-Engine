b8d8f9544dd152904d0e0d200b2152ef41a0b1f2
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-03-15 00:09:51 +0100

    Scene Manager more function implementations

--- CoffeeEngine/src/CoffeeEngine/Scene/SceneManager.h
+++ CoffeeEngine/src/CoffeeEngine/Scene/SceneManager.h
@@ -9,20 +9,33 @@ namespace Coffee {
     class SceneManager
     {
     public:
+        enum class SceneState
+        {
+            Edit = 0,
+            Play = 1
+        };
+    public:
         
         static Ref<Scene> PreloadScene(const std::filesystem::path& scenePath);
         static std::future<Ref<Scene>> PreloadSceneAsync(const std::filesystem::path& scenePath);
 
         static void ChangeScene(const std::filesystem::path& scenePath);
         static void ChangeScene(const Ref<Scene>& scene);
-        /* static void ChangeSceneAsync(const std::filesystem::path& scenePath); */
+        static void ChangeSceneAsync(const std::filesystem::path& scenePath);
 
-        static Ref<Scene>& GetActiveScene() { return m_ActiveScene; }
+        static Ref<Scene>& GetActiveScene() { return s_ActiveScene; }
 
         static void SetWorkingDirectory(const std::filesystem::path& workingDirectory) { s_WorkingDirectory = workingDirectory; }
+
+        static void SetSceneState(SceneState state) { s_SceneState = state; }
+        static SceneState GetSceneState() { return s_SceneState; }
     private:
+        static void ExitCurrentScene();
+        static void InitNewScene();
+    private:
+        static SceneState s_SceneState;
         static std::filesystem::path s_WorkingDirectory;
-        static Ref<Scene> m_ActiveScene;
+        static Ref<Scene> s_ActiveScene;
     };
 
 }
