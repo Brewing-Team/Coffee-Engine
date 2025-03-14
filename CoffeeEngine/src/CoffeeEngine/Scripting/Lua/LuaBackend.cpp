e9a6b822047650c329a5283ac69e666c6d9c032d
mdoradom <mariodoradomartinez@gmail.com>
2025-03-15 00:17:19 +0100

    timer and stopwatch bindings for lua

--- CoffeeEngine/src/CoffeeEngine/Scripting/Lua/LuaBackend.cpp
+++ CoffeeEngine/src/CoffeeEngine/Scripting/Lua/LuaBackend.cpp
@@ -1,16 +1,19 @@
 #include "LuaBackend.h"
 
+#include "CoffeeEngine/Core/ControllerCodes.h"
 #include "CoffeeEngine/Core/Input.h"
 #include "CoffeeEngine/Core/KeyCodes.h"
-#include "CoffeeEngine/Core/ControllerCodes.h"
 #include "CoffeeEngine/Core/Log.h"
 #include "CoffeeEngine/Core/MouseCodes.h"
-#include <fstream>
-#include <lua.h>
-#include <regex>
+#include "CoffeeEngine/Core/Timer.h"
+#include "CoffeeEngine/Core/Stopwatch.h"
+
 #include "CoffeeEngine/Scene/Components.h"
 #include "CoffeeEngine/Scene/Entity.h"
 #include "CoffeeEngine/Scripting/Lua/LuaScript.h"
+#include <fstream>
+#include <lua.h>
+#include <regex>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -406,7 +409,53 @@ namespace Coffee {
         luaState["Input"] = inputTable;
         # pragma endregion
 
-        # pragma region Bind Timer Functions
+        # pragma region Bind Timer and Stopwatch Functions
+
+        // Bind Stopwatch class
+        luaState.new_usertype<Stopwatch>("Stopwatch",
+            sol::constructors<Stopwatch()>(),
+            "start", &Stopwatch::Start,
+            "stop", &Stopwatch::Stop,
+            "reset", &Stopwatch::Reset,
+            "get_elapsed_time", &Stopwatch::GetElapsedTime,
+            "get_precise_elapsed_time", &Stopwatch::GetPreciseElapsedTime
+        );
+
+        // Bind Timer class
+        luaState.new_usertype<Timer>("Timer",
+            sol::constructors<Timer(), Timer(double, bool, bool, Timer::TimerCallback)>(),
+            "start", &Timer::Start,
+            "stop", &Timer::Stop,
+            "set_wait_time", &Timer::setWaitTime,
+            "get_wait_time", &Timer::getWaitTime,
+            "set_one_shot", &Timer::setOneShot,
+            "is_one_shot", &Timer::isOneShot,
+            "set_auto_start", &Timer::setAutoStart,
+            "is_auto_start", &Timer::isAutoStart,
+            "set_paused", &Timer::setPaused,
+            "is_paused", &Timer::isPaused,
+            "get_time_left", &Timer::GetTimeLeft,
+            "set_callback", [](Timer& timer, const sol::protected_function& callback) {
+                timer.SetCallback([callback] {
+                    if (const auto result = callback(); !result.valid()) {
+                        const sol::error err = result;
+                        COFFEE_CORE_ERROR("Timer callback error: {0}", err.what());
+                    }
+                });
+            }
+        );
+
+        // Helper function to create a timer
+        luaState.set_function("create_timer", [](double waitTime, bool autoStart, bool oneShot, sol::protected_function callback) {
+            return Timer(waitTime, autoStart, oneShot, [callback]() {
+                auto result = callback();
+                if (!result.valid()) {
+                    sol::error err = result;
+                    COFFEE_CORE_ERROR("Timer callback error: {0}", err.what());
+                }
+            });
+        });
+
         # pragma endregion
 
         # pragma region Bind GLM Functions
