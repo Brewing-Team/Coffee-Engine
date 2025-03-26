c6a5c5a7312cc3fd1dbb00b7925008769748ff84
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2025-03-26 17:24:45 +0100

    serialize ActiveComponent

--- CoffeeEngine/src/CoffeeEngine/Scene/Components.h
+++ CoffeeEngine/src/CoffeeEngine/Scene/Components.h
@@ -920,6 +920,19 @@
     {
         ActiveComponent() = default;
         ActiveComponent(const ActiveComponent&) = default;
+
+        template<class Archive>
+        void save (Archive& archive) const
+        {
+            archive(cereal::make_nvp("Active", true));
+        }
+
+        template<class Archive>
+        void load (Archive& archive)
+        {
+            bool active;
+            archive(cereal::make_nvp("Active", active));
+        }
     };
  }
  
