7a28a484c9c10a52c6bebd918f5e884ab1dd6afb
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-04-20 16:42:02 +0200

    Diffuse Irradiance done (WIP)

--- CoffeeEngine/src/CoffeeEngine/Renderer/RendererAPI.cpp
+++ CoffeeEngine/src/CoffeeEngine/Renderer/RendererAPI.cpp
@@ -49,6 +49,8 @@ namespace Coffee {
 		glCullFace(GL_BACK);
 
 		glDepthFunc(GL_LEQUAL);
+
+		glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
     }
 
 	void RendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
