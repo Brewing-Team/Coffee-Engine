470d5eff151c564bbeaa2f2cc3638485aaa2abbc
Roger Puchol Celma <49079760+Ropuce@users.noreply.github.com>
2025-04-30 16:27:36 +0200

    Fixed #147

--- CoffeeEditor/src/EditorLayer.cpp
+++ CoffeeEditor/src/EditorLayer.cpp
@@ -185,7 +185,8 @@ namespace Coffee {
 
                     Entity hoveredEntity = entityID == 16777215 ? Entity() : Entity((entt::entity)entityID, SceneManager::GetActiveScene().get());
 
-                    m_SceneTreePanel.SetSelectedEntity(hoveredEntity);
+                    if (hoveredEntity.IsValid())
+                        m_SceneTreePanel.SetSelectedEntity(hoveredEntity);
                 }
             }
         }
