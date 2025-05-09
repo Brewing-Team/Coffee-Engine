455065a2b6e0685a9e63c54a31ca02046c08597d
Roger Puchol Celma <49079760+ropuce@users.noreply.github.com>
2025-05-09 21:22:04 +0200

    Default scene in project
    Added method to project to load its default scene if it exists
    
    Changed Runtme entry scene logic to take advantage of this
    
    Fixes towards preventing buffer-related errors in Project Settings
    
    If no default scene has been manually defined in project, default to original default (Default.TeaScene on project root)
    
    Fixed project name being used as filename when saving

--- CoffeeEditor/Panels/ProjectSettingsPanel.cpp
+++ CoffeeEditor/Panels/ProjectSettingsPanel.cpp
@@ -240,8 +240,9 @@ namespace Coffee {
         if (m_RefreshPanels & PanelDisplayEnum::General)
         {
             // Refresh values (in case of new project loaded)
-            m_NewProjectName = Project::GetProjectName();
-            m_NewProjectName.reserve(256);
+            std::string s = Project::GetProjectName();
+            m_NewProjectName.fill('\0');
+            std::copy_n(s.begin(), min(s.size(), 255),m_NewProjectName.begin());
 
             m_RefreshPanels ^= PanelDisplayEnum::General;
         }
@@ -253,22 +254,35 @@ namespace Coffee {
         char* strData = str.data();
         ImGui::InputText("##AudioBanksPath", strData, str.size(), ImGuiInputTextFlags_ReadOnly);
         ImGui::SameLine();
-        if (ImGui::Button("Select...##AudioBanksPathButton"))
+        if (ImGui::InputText("##gamename", m_NewProjectName.data(), 255, ImGuiInputTextFlags_EnterReturnsTrue))
+        {
+            // no need to check for active project, this window is only accessible when a project is active
+            // NOT a redundant call to c_str(). It is being used to trim extra null characters at the end of the string
+            Project::SetProjectName(std::string(m_NewProjectName.begin(), m_NewProjectName.end()).c_str());
+        }
+
+        std::string defScenePath = Project::GetProjectDefaultScene().string();
+        ImGui::Text("Default scene path: ");
+        ImGui::SameLine();
+        ImGui::InputText("##DefaultScenePath", defScenePath.data(), defScenePath.size(), ImGuiInputTextFlags_ReadOnly);
+        ImGui::SameLine();
+        if (ImGui::Button("Select...##DefaultScenePathButton"))
         {
             FileDialogArgs args;
             args.DefaultPath = Project::GetProjectDirectory().string();
-            std::filesystem::path path = FileDialog::PickFolder(args);
-            if (is_directory(path))
+            args.Filters.push_back({"Coffee Engine Scene", "TeaScene"});
+            std::filesystem::path path = FileDialog::OpenFile(args);
+            if (!path.empty())
             {
                 path = std::filesystem::relative(path, Project::GetProjectDirectory());
-                Project::SetRelativeAudioDirectory(path);
-                Audio::OnProjectLoad();
+                Project::SetProjectDefaultScene(path);
             }
         }
 
-        std::string str = Coffee::Project::GetRelativeAudioDirectory().string();
-        char* strData = str.data();
-        ImGui::InputText("##AudioBanksPath", strData, str.size(), ImGuiInputTextFlags_ReadOnly);
+        std::string audioDirPath = Project::GetRelativeAudioDirectory().string();
+        ImGui::Text("Audio directory: ");
+        ImGui::SameLine();
+        ImGui::InputText("##AudioBanksPath", audioDirPath.data(), audioDirPath.size(), ImGuiInputTextFlags_ReadOnly);
         ImGui::SameLine();
         if (ImGui::Button("Select...##AudioBanksPathButton"))
         {
